99bf9fec9b26e29819f0738a2b46789e1c7b26d4
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-13 18:48:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function shout(string){
-  console.log (string.toUpperCase())
+  return string.toUpperCase()
 }
 
+shout('hello')
+
 function whisper(string){
-  console.log (string.toLowerCase())
+  return string.toLowerCase()
 }
+
+whisper('hello')
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+logShout()
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+logWhisper()
 
 function sayHiToGrandma(string){
   return string.toLowerCase()
 }
+
+sayHiToGrandma()
+
+function sayHiToGrandma(string){
+  console.log(string.toUpperCase())
+}
+
+sayHiToGrandma()
+
+
+function sayHiToGrandma(string){
+  return string
+}
+
+sayHiToGrandma()
