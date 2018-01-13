b76be852a9918423d8d9e423ceb4ec3892330418
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-13 19:07:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,20 +23,13 @@ function logWhisper(string){
 logWhisper()
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string)
+  if (string.toLowerCase() === string){
   return "I can't here you!"
+} else if (string.toUpperCase() === string) {
+  return "YES INDEED!"
+}else{ return "I love you, too"
+  
+}
 }
 
-sayHiToGrandma("I love you, too.")
-
-function sayHiToGrandma(string){
-  return string.toUpperCase()
-}
-
-sayHiToGrandma("YES INDEED!")
-
-function sayHiToGrandma(string){
-  return "I love you, too."
-}
-
-sayHiToGrandma('I love you, Grandma.')
+sayHiToGrandma("I love you, Grandma.")
