46555a5b093afff712470cf361f2520bb050fc45
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-13 19:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ logWhisper()
 
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
-  return "I can't here you!"
+  return "I can't hear you!"
 } else if (string.toUpperCase() === string) {
   return "YES INDEED!"
 }else{ return "I love you, too."
